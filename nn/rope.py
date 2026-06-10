7e70227e21b844e244a8cbf11286423c89a23035
AndPotap <apotapczynski@gmail.com>
2026-06-09 20:19:16 -0400

    Finished adding my RoPE implementation

--- nn/rope.py
+++ nn/rope.py
@@ -5,8 +5,18 @@ from torch.nn import Module
 class RoPE(Module):
     def __init__(self, embd: int, max_seq: int):
         super().__init__()
-        theta = 10 ** -(4 * torch.arange(embd) / embd)
+        assert embd % 2 == 0, f"{embd=} is not divisible by 2"
+        embd_half = embd // 2
+        theta = 10**-(4.0 * torch.arange(embd_half) / embd_half)
         theta = torch.arange(max_seq)[:, None] * theta[None, :]
+        self.register_buffer("cos", torch.cos(theta)[None], persistent=False)
+        self.register_buffer("sin", torch.sin(theta)[None], persistent=False)
 
-    def forward(x):
-        return x
+    def forward(self, x):
+        # x: [...,S,2D]
+        x_even = x[..., 0::2]
+        x_odd = x[..., 1::2]
+        z1 = self.cos * x_even - self.sin * x_odd
+        z2 = self.sin * x_even + self.cos * x_odd
+        z = torch.stack([z1, z2], dim=-1)  # [B,S,D,2]
+        return z.flatten(-2)  # [B,S,2D]
--- tests/nn/test_rope.py
+++ tests/nn/test_rope.py
@@ -1,7 +1,89 @@
+import torch
+import numpy as np
+
 from nn.rope import RoPE
 
 
 def test_rope():
-    S, D = 4, 3
-    model = RoPE(embd=D, max_seq=S)
-    assert model.cos.shape == (S, D)
+    # B, S, D = 1, 4, 6
+    # B, S, D = 7, 32, 64
+    B, S, D = 7, 512, 128
+    rope = RoPE(embd=D, max_seq=S)
+    assert rope.cos.shape == (1, S, D // 2)
+    assert rope.sin.shape == (1, S, D // 2)
+
+    x = torch.randn(B, S, D)
+    z = rope(x)
+    assert z.shape == (B, S, D)
+
+    dtype = torch.float64
+    x = x.to(dtype)
+    norm_x = torch.linalg.norm(x)
+    rope.to(dtype)
+    norm_z = torch.linalg.norm(rope(x))
+    abs_diff = torch.abs(norm_x - norm_z)
+    print(f"{abs_diff=:1.3e}")
+    assert abs_diff < 1e-6
+
+    x_np = x.numpy()
+    approx = rope_np_fast(x_np)
+    ans = rope_np(x_np)
+    abs_diff = np.max(np.abs(approx - ans))
+    print(f"{abs_diff=:1.3e}")
+    assert abs_diff < 1e-12
+
+    ans = rope_np(x_np)
+    approx = rope(x).numpy()
+    abs_diff = np.max(np.abs(approx - ans))
+    print(f"{abs_diff=:1.3e}")
+    assert abs_diff < 1e-4
+
+
+def rope_np(x):
+    dtype = np.float128
+    B, S, D = x.shape
+    D2 = D // 2
+    x = x.astype(dtype)
+
+    phi = np.zeros(shape=(S, D2), dtype=dtype)
+    R = np.zeros(shape=(S, D, D), dtype=dtype)
+    z = np.empty(shape=(B, S, D), dtype=dtype)
+
+    for s in range(S):
+        for d in range(D2):
+            phi[s, d] = s * 10**(-4. * (2. * (d / D)))
+
+    for s in range(S):
+        for idx in range(0, D, 2):
+            d = idx // 2
+            R[s, idx, idx] = np.cos(phi[s, d])
+            R[s, idx, idx + 1] = -np.sin(phi[s, d])
+            R[s, idx + 1, idx] = np.sin(phi[s, d])
+            R[s, idx + 1, idx + 1] = np.cos(phi[s, d])
+
+    for b in range(B):
+        for s in range(S):
+            z[b, s] = R[s] @ x[b, s]
+
+    return z.astype(np.float64)
+
+
+def rope_np_fast(x):
+    dtype = np.float128
+    B, S, D = x.shape
+    D2 = D // 2
+    x = x.astype(dtype)
+
+    theta = 10**-(4 * np.arange(D2, dtype=dtype) / D2)
+    phi = np.arange(S, dtype=dtype)[:, None] * theta[None, :]
+    cos = np.cos(phi)
+    sin = np.sin(phi)
+
+    x_even = x[..., 0::2]
+    x_odd = x[..., 1::2]
+    z1 = cos * x_even - sin * x_odd
+    z2 = sin * x_even + cos * x_odd
+    z = np.stack([z1, z2], axis=-1)  # [B,S,D,2]
+    z = z.reshape(B, S, D)
+
+    return z.astype(np.float64)
